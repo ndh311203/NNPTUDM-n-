f8c9aa77bcceacb5f92e2e628e955e499ec6f835
Nguyen Duc Huy <ndh311203@gmail.com>
2026-04-04 22:52:25 +0700

    Cleanup: Remove C# code, keep only Node.js API

--- spatc/spatc/Models/DichVuIndexViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace spatc.Models
-{
-    public class DichVuIndexViewModel
-    {
-        public List<DichVu> Grooming { get; set; } = new List<DichVu>();
-        public List<DichVu> KhachSan { get; set; } = new List<DichVu>();
-    }
-}
-
